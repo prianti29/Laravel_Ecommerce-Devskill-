fb3c51d5fe5a6a65bfe6e562f155641946be7ae7
prianti29 <59369461+prianti29@users.noreply.github.com>
2022-01-24 00:10:27 +0600

    adminELT uploaded

---
+++ resources/views/admin/layouts/nav.blade.php
@@ -0,0 +1,32 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+        data-accordion="false">
+  
+        {{-- <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    Dashboard
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{asset("/admin/index.html") }}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v1</p>
+                    </a>
+                </li>
+             
+            </ul>
+        </li> --}}
+        
+        <li class="nav-item">
+            <a href="{{asset("/admin/iframe.html") }}" class="nav-link">
+                <i class="nav-icon fas fa-ellipsis-h"></i>
+                <p>Tabbed IFrame Plugin</p>
+            </a>
+        </li>
+</nav>
+<!-- /.sidebar-menu -->
+</div>
